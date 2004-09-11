b3b24defeac18ab2f4277a48b124695b8a0cace2
Gabor Hojtsy <goba@php.net>
2004-09-11 12:35:18 +0000

    the site was rebranded as 'Documentation Tools' site recently; and a typo

--- www/copyright.php
+++ www/copyright.php
@@ -7,7 +7,7 @@ echo site_header('Copyright');
 <h2>Source Code Copyright</h2>
 
 <p>
- The PHP Documentation Teams Website source code is available under the
+ The PHP Documentation Tools Website source code is available under the
  <a href="http://php.net/license/">PHP License</a>. See the notices in the
  source code for more information and an authors list.
 </p>
@@ -15,7 +15,7 @@ echo site_header('Copyright');
 <h2>Documentation Copyright</h2>
 
 <p>
- The documentation of the different projects we tie together is under
+ The documentation of the different projects we tie together are under
  different licenses, so you need to check the copyright notices in the
  respective manuals for more information. In case you have questions not
  handled in the licenses, please ask at
