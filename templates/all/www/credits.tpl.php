fd1271f6372bd8f1c047aa60decac300cacc9f44
Sean Coates <sean@php.net>
2004-11-12 23:13:47 +0000

    credits->template

---
+++ templates/all/www/credits.tpl.php
@@ -0,0 +1,19 @@
+<p>
+ &docweb.credits.subhead;
+</p>
+
+<dl>
+ <dt>&docweb.credits.section.admin;</dt>
+ <dd>Jacques Marneweck</dd>
+ <dt>&docweb.credits.section.php-services;</dt>
+ <dd>&docweb.credits.php-systems;</dd>
+ <dt>&docweb.credits.section.devs;</dt>
+ <dd>
+   Mehdi Achour, Vincent Gevers, Gabor Hojtsy,
+   Yannick Torres &docweb.common.and; Sean Coates</dd>
+</dl>
+
+<p>
+ &docweb.credits.section.icons;
+ <a href="http://jimmac.musichall.cz/">Jakub 'jimmac' Steiner</a>.
+</p>
--- www/credits.php
+++ www/credits.php
@@ -2,27 +2,7 @@
 /* $Id$ */
 
 include '../include/init.inc.php';
-echo site_header('Website credits');
+echo site_header('docweb.common.header.credits');
+echo DocWeb_Template::get('credits.tpl.php');
+echo site_footer();
 ?>
-<p>
- The following people helped to make this site a possibility.
- We are very thankful for their work.
-</p>
-
-<dl>
- <dt>Server administration</dt>
- <dd>Jacques Marneweck</dd>
- <dt>PHP.net services (CVS, mailing list, etc)</dt>
- <dd>PHP.net system administration team</dd>
- <dt>Website development</dt>
- <dd>
-   Mehdi Achour, Vincent Gevers, Gabor Hojtsy,
-   Yannick Torres and Sean Coates</dd>
-</dl>
-
-<p>
- We have also used some fine icons created by
- <a href="http://jimmac.musichall.cz/">Jakub 'jimmac' Steiner</a>.
-</p>
-
-<?php echo site_footer(); ?>
