64ef08fd5a6c90bcc863230ff50f42dd4a0a02f5
Sean Coates <sean@php.net>
2004-11-13 01:52:33 +0000

    copyright->template

---
+++ templates/all/www/copyright.tpl.php
@@ -0,0 +1,12 @@
+<h2>&docweb.copyright.section.source;</h2>
+
+<p>
+ &docweb.copyright.source;
+</p>
+
+<h2>&docweb.copyright.section.doc;</h2>
+
+<p>
+ &docweb.copyright.doc;
+</p>
+
--- www/copyright.php
+++ www/copyright.php
@@ -2,24 +2,7 @@
 /* $Id$ */
 
 include_once '../include/init.inc.php';
-echo site_header('Copyright');
+echo site_header('docweb.common.header.copyright');
+echo DocWeb_Template::get('copyright.tpl.php');
+echo site_footer();
 ?>
-<h2>Source Code Copyright</h2>
-
-<p>
- The PHP Documentation Tools Website source code is available under the
- <a href="http://php.net/license/">PHP License</a>. See the notices in the
- source code for more information and an authors list.
-</p>
-
-<h2>Documentation Copyright</h2>
-
-<p>
- The documentation of the different projects we tie together are under
- different licenses, so you need to check the copyright notices in the
- respective manuals for more information. In case you have questions not
- handled in the licenses, please ask at
- <a href="mailto:doc-license@lists.php.net">doc-license@lists.php.net</a>.
-</p>
-
-<?php echo site_footer(); ?>
