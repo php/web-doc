c235d408f0ee938efa235751f77a7ed373d430fb
Jacques Marneweck <jacques@php.net>
2004-12-14 14:15:02 +0000

    Fix to use varialbes from build-ops cache (ala configure).

--- scripts/generation.sh
+++ scripts/generation.sh
@@ -1,38 +1,40 @@
 #!/bin/sh
 
+. ../build-ops
+
 echo "Generating revcheck databases"
 
 # cd back again 
-cd ../scripts
+cd ${SCRIPTSDIR}
 
 # PHP
 echo "Generating PHP database"
-php -q ./rev.php php
+${PHP} -q ./rev.php php
 echo "... done."
 echo "Generating PHP pictures"
-php -q ./gen_picture_info.php php
+${PHP} -q ./gen_picture_info.php php
 echo "... done"
 
 
 # PEAR
 echo "Generating PEAR database"
-php -q ./rev.php pear
+${PHP} -q ./rev.php pear
 echo "... done."
 echo "Generating PEAR pictures"
-php -q ./gen_picture_info.php pear
+${PHP} -q ./gen_picture_info.php pear
 echo "... done"
 
 
 # SMARTY
 echo "Generating SMARTY database"
-php -q ./rev.php smarty
+${PHP} -q ./rev.php smarty
 echo "... done."
 echo "Generating SMARTY pictures"
-php -q ./gen_picture_info.php smarty
+${PHP} -q ./gen_picture_info.php smarty
 echo "... done"
 
 
 
 echo "Generating global graphs"
-php -q ./gen_picture_info_all_lang.php
+${PHP} -q ./gen_picture_info_all_lang.php
 echo "... done"
