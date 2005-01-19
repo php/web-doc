db91b1e53375f4f7954d48cad7fcb40d7e8c89fd
Vincent Gevers <vincent@php.net>
2005-01-19 16:50:47 +0000

    Template the error page

--- www/error.php
+++ www/error.php
@@ -3,28 +3,7 @@
 
 include '../include/init.inc.php';
 
-$red_status = isset($_SERVER["REDIRECT_STATUS"]) ? $_SERVER["REDIRECT_STATUS"] : '';
-$title = 'Apache :: Error ' . $red_status;
-echo site_header($title);
-?>
-<h2><?php echo $title; ?></h2>
-<p>
-<?php 
-if (isset($_SERVER['REDIRECT_ERROR_NOTES'])) {
-    echo $_SERVER['REDIRECT_ERROR_NOTES'];
-} elseif (isset($_SERVER['REDIRECT_REDIRECT_ERROR_NOTES'])) {
-    echo $_SERVER['REDIRECT_REDIRECT_ERROR_NOTES'];
-} else {
-    if ($red_status == 404) {
-        echo 'File Not Found: ' . htmlspecialchars($_SERVER['REQUEST_URI']);
-    } else {
-        echo 'No message available.';
-    }
-}
-
-
-?>
-</p>
-<?php
+echo site_header('docweb.common.header.error');
+echo DocWeb_Template::get('error.tpl.php');
 echo site_footer();
 ?>
