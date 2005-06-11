5fed15355ec377bc57084145662bcc8a4d0b16e2
Nuno Lopes <nlopess@php.net>
2005-06-11 12:02:00 +0000

    link extensions and functions to php.net

--- templates/all/www/missingexamples.tpl.php
+++ templates/all/www/missingexamples.tpl.php
@@ -4,10 +4,10 @@ There are <?php echo $missingEgCount; ?> functions that are missing examples.
 <table>
  <?php foreach ($missingEgData as $name => $ext) { ?>
   <tr>
-   <th><?php echo $name.' ('.count($ext).')'; ?></th>
+   <th><a href="http://php.net/<?php echo $name ?>"><?php echo $name.'</a> ('.count($ext).')'; ?></th>
   </tr>
   <?php foreach ($ext as $function) { ?>
-  <tr><td valign="top"><?php echo $function; ?></td></tr>
+  <tr><td valign="top"><a href="http://php.net/<?php echo $function; ?>"><?php echo $function; ?></a></td></tr>
   <?php } ?>
  <?php } ?>
 </table>
--- templates/all/www/undocfuncs.tpl.php
+++ templates/all/www/undocfuncs.tpl.php
@@ -4,10 +4,10 @@ There are <?php echo $undocCount; ?> undocumented functions.
 <table>
  <?php foreach ($undocData as $name => $ext) { ?>
   <tr>
-   <th><?php echo $name.' ('.count($ext).')'; ?></th>
+   <th><a href="http://php.net/<?php echo $name ?>"><?php echo $name.'</a> ('.count($ext).')'; ?></th>
   </tr>
   <?php foreach ($ext as $function) { ?>
-  <tr><td valign="top"><?php echo $function; ?></td></tr>
+  <tr><td valign="top"><a href="http://php.net/<?php echo $function; ?>"><?php echo $function; ?></a></td></tr>
   <?php } ?>
  <?php } ?>
 </table>
