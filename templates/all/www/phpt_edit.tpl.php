b7dfc671660bbc12f5a43c34358c7ede52e28939
Etienne Kneuss <colder@php.net>
2007-01-18 19:16:21 +0000

    Drop heredoc as it's not convenient with all the tests

--- templates/all/www/phpt_edit.tpl.php
+++ templates/all/www/phpt_edit.tpl.php
@@ -1,11 +1,10 @@
 <?php
 if (!empty($error)) {
 
-    echo <<<HTML
+    echo '
 <h2>Error:</h2>
-$error;
-<br /><br /><a href="phpt_generator.php?q={$q}">&docweb.phpt.return;</a>
-HTML;
+'.$error.'
+<br /><br /><a href="phpt_generator.php?q='.$q.'">&docweb.phpt.return;</a>';
 
     return;
 }
@@ -16,29 +15,32 @@ if ($edited === false) {
     echo '<h2>&docweb.phpt.edit.ok;</h2>';
 }
 
-echo <<<HTML
+echo '
 <h1>&docweb.phpt.edit;</h1>
 
-<a href="phpt_generator.php?q={$q}">&docweb.phpt.return;</a><br />
-<a href="phpt_generator.php?generateId={$test['id']}">&docweb.phpt.generate;</a><br />
+<a href="phpt_generator.php?q='.$q.'">&docweb.phpt.return;</a><br />
+<a href="phpt_generator.php?generateId='.$test['id'].'">&docweb.phpt.generate;</a><br />
 
 <form action="" method="POST" class="phpt">
   <h2>&docweb.phpt.edit.title;</h2>
-  <input name="title" type="text" value="{$test['title']}" size="80" />
+  <input name="title" type="text" value="'.$test['title'].'" size="80" />
   
   <h2>&docweb.phpt.edit.skipif;</h2>
-  <textarea name="skipif" rows="{$test['skipif_lines']}" cols="80">{$test['skipif']}</textarea>
+  <textarea name="skipif" rows="'.$test['skipif_lines'].'" cols="80">'.$test['skipif'].'</textarea>
   
   <h2>&docweb.phpt.edit.test;</h2>
-  <textarea name="test" rows="{$test['test_lines']}" cols="80">{$test['test']}</textarea>
+  <textarea name="test" rows="'.$test['test_lines'].'" cols="80">'.$test['test'].'</textarea>
 
   <h2>&docweb.phpt.edit.expected;</h2>
-  <textarea name="expected" rows="{$test['expected_lines']}" cols="80">{$test['expected']}</textarea>
+  <textarea name="expected" rows="'.$test['expected_lines'].'" cols="80">'.$test['expected'].'</textarea>
 
   <h2>&docweb.phpt.edit.approve;</h2>
-  <input name="approve" type="checkbox"{$test['approve_checkbox']}/>
-  <input type="submit" name="commit" value="&docweb.phpt.commit;">
-</form>
-HTML;
+  <input name="approve" type="checkbox"'.$test['approve_checkbox'].'/>
+';
+if(is_admin()) {
+    echo '  <input type="submit" name="commit" value="&docweb.phpt.commit;">';
+}  
+echo '
+    </form>';
 
 ?>
--- templates/all/www/phpt_list.tpl.php
+++ templates/all/www/phpt_list.tpl.php
@@ -6,54 +6,63 @@ if ($ntotal == 200) {
     $limitError = '';
 }
 
-echo <<<HTML
+echo '
+<h1>&docweb.phpt.list;</h1>';
 
-<h1>&docweb.phpt.list;</h1>
+if(is_admin()) {
+    echo '
+<a href="?import">&docweb.phpt.import;</a><br />';
+}
 
-<a href="?import">&docweb.phpt.import;</a><br />
+echo '
 <a href="?generateAll">&docweb.phpt.generateall;</a><br /><br />
 
 <form action="phpt_generator.php" method="post" class="phpt">
 
-<input type="text" name="q" value="{$search}" style="display:inline; margin: 0px;" /> <input type="submit" name="search" value="&docweb.phpt.search;"/>
-HTML;
+<input type="text" name="q" value="'.$search.'" style="display:inline; margin: 0px;" /> <input type="submit" name="search" value="&docweb.phpt.search;"/>';
 
 if (!empty($tests)) {
 
-    echo <<<HTML
+    echo '
 <br /><br /><br />
 
-<input type="submit" name="generate" value="&docweb.phpt.generate;"/> <input type="submit" name="delete" value="&docweb.phpt.delete;"/><br /><br />
- {$limitError}Stats: $ntotal examples, $napproved approved, $nfilled filled, $nimported imported.<br />
+<input type="submit" name="generate" value="&docweb.phpt.generate;"/> ';
+
+    if (is_admin()) {
+        echo '<input type="submit" name="delete" value="&docweb.phpt.delete;"/>';
+    }
+
+    echo '
+<br /><br />
+ '.$limitError.'Stats: '.$ntotal.' examples, '.$napproved.' approved, '.$nfilled.' filled, '.$nimported.' imported.<br />
  <table border="1">
   <tr>
    <th>&docweb.phpt.list.id;</th>
    <th>&docweb.phpt.list.location;</th>
    <th>&docweb.phpt.list.title;</th>
    <th>&docweb.phpt.list.status;</th>
-  </tr>
-HTML;
+  </tr>';
 
 
     foreach ($tests as $test) {
 
-        echo <<<HTML
-
-  <tr class="{$test['class']}">
-   <td><input type="checkbox" name="ids[{$test['id']}]" /> <a href="?editId={$test['id']}&q={$search_enc}" title="Edit">{$test['id']}</a></td>
-   <td><a href="{$test['cvs_link']}" target="_blank">{$test['location']}</a></td>
-   <td>{$test['title_limited']}</td>
-   <td>{$test['status']}</td>
-  </tr>
-HTML;
+        echo '
+  <tr class="'.$test['class'].'">
+   <td><input type="checkbox" name="ids['.$test['id'].']" /> <a href="?editId='.$test['id'].'&q='.$search_enc.'" title="Edit">'.$test['id'].'</a></td>
+   <td><a href="'.$test['cvs_link'].'" target="_blank">'.$test['location'].'</a></td>
+   <td>'.$test['title_limited'].'</td>
+   <td>'.$test['status'].'</td>
+  </tr>';
 
     }
 
-echo <<<HTML
+    echo '
  </table>
  
- <br /><input type="submit" name="generate" value="&docweb.phpt.generate;"/> <input type="submit" name="delete" value="&docweb.phpt.delete;"/>
-HTML;
+ <br /><input type="submit" name="generate" value="&docweb.phpt.generate;"/> ';
+    if (is_admin()) {
+        echo '<input type="submit" name="delete" value="&docweb.phpt.delete;"/>';
+    }
 
 }
 ?>
