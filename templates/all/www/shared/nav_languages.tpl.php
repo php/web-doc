0a2b64c9e99567bb09f33b504c668f0ef4c56d0a
Philip Olson <philip@php.net>
2008-06-05 17:45:23 +0000

    Choose a specific country flag when the language code involves many countries And added the Iran flag (ir) for Persian (fa)

--- templates/all/www/shared/nav_languages.tpl.php
+++ templates/all/www/shared/nav_languages.tpl.php
@@ -4,8 +4,15 @@ foreach ($languages as $code => $name) {
         echo '<a href="' . get_insite_address(NULL, 'all') . '">&docweb.common.all;</a> '."\n";
     }
     else {
+
+        // Show specific country flags when language/country codes differ
+        $alt_flags = array(
+            'fa' => 'ir',
+        );
+        $flag_code = isset($alt_flags[$code]) ? $alt_flags[$code] : $code;
+
         // @@@ TODO: translate/entity names
-        echo '<a href="' . get_insite_address(NULL, $code) . '" title="' . $name . '" ><img src="/images/flags/' . $code . '.png" alt="' . $name . '" /></a>'."\n";
+        echo '<a href="' . get_insite_address(NULL, $code) . '" title="' . $name . '" ><img src="/images/flags/' . $flag_code . '.png" alt="' . $name . '" /></a>'."\n";
     }
 }
 ?>
