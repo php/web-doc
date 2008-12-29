d7dbb68ef19003b807eddd8eeb6bca70ab0079e7
Philip Olson <philip@php.net>
2008-12-29 22:13:05 +0000

    Added reviewedcheck.php as requested by Yannick

--- scripts/gen_translation_info.sh
+++ scripts/gen_translation_info.sh
@@ -14,5 +14,6 @@ for LANG in $LANGS; do
   $PHP -n -d error_reporting=0 scripts/revcheck.php ${LANG}                > $TRANSPATH/revcheck_${LANG}.html
   $PHP -n -d error_reporting=0 scripts/check-trans-maint.php -n -l ${LANG} -m 1 > $TRANSPATH/maintainer_${LANG}.html
   $PHP -n -d error_reporting=0 scripts/check-trans-params.php ${LANG} 1 1  > $TRANSPATH/params_${LANG}.html
+  $PHP -n -d error_reporting=0 scripts/reviewedcheck.php ${LANG}           > $TRANSPATH/reviewedcheck_${LANG}.html
 
 done
