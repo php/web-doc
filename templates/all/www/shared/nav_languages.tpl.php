c5fed5abdf01bcd99b1e7d353f5ae27a0bb6b046
anatoly techtonik <techtonik@php.net>
2009-09-10 09:59:43 +0000

    + white background for active subsite flag

--- templates/all/www/shared/nav_languages.tpl.php
+++ templates/all/www/shared/nav_languages.tpl.php
@@ -1,7 +1,15 @@
 <?php
+echo "&nbsp;"; // required for proper positioning of the active background
 foreach ($languages as $code => $name) {
+
+    if (LANGC == $code) {
+        echo '<span class="active"></span><a class="active" ';
+    } else {
+        echo '<a ';
+    }
+
     if ($code == 'all') {
-        echo '<a href="' . get_insite_address(NULL, 'all') . '">&docweb.common.all;</a> '."\n";
+        echo 'href="' . get_insite_address(NULL, 'all') . '">&docweb.common.all;</a> '."\n";
     }
     else {
 
@@ -12,7 +20,8 @@ foreach ($languages as $code => $name) {
         $flag_code = isset($alt_flags[$code]) ? $alt_flags[$code] : $code;
 
         // @@@ TODO: translate/entity names
-        echo '<a href="' . get_insite_address(NULL, $code) . '" title="' . $name . '" ><img src="/images/flags/' . $flag_code . '.png" alt="' . $name . '" /></a>'."\n";
+        
+        echo 'href="' . get_insite_address(NULL, $code) . '" title="' . $name . '" ><img src="/images/flags/' . $flag_code . '.png" alt="' . $name . '" /></a>'."\n";
     }
 }
 ?>
