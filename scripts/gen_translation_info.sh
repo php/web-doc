2627fa6b6629425cb546744962949a26539099f7
Shein Alexey <shein@php.net>
2011-02-24 10:21:29 +0000

    Added generation of extended version of revcheck script (with uptodate files).

--- scripts/gen_translation_info.sh
+++ scripts/gen_translation_info.sh
@@ -11,6 +11,7 @@ cd phpdoc-all
 for LANG in $LANGS; do
   $PHP doc-base/scripts/check-trans.php ${LANG}             > $TRANSPATH/transcheck_${LANG}.html
   $PHP -derror_reporting=0 doc-base/scripts/revcheck.php ${LANG}                > $TRANSPATH/revcheck_${LANG}.html
+  $PHP -derror_reporting=0 doc-base/scripts/revcheck.php ${LANG} --show-uptodate > $TRANSPATH/revcheck_extended_${LANG}.html
   $PHP doc-base/scripts/check-trans-maint.php -n -l ${LANG} -m 1 > $TRANSPATH/maintainer_${LANG}.html
   $PHP -derror_reporting=0 doc-base/scripts/check-trans-params.php ${LANG} 1 1  > $TRANSPATH/params_${LANG}.html
   $PHP doc-base/scripts/reviewedcheck.php ${LANG}           > $TRANSPATH/reviewedcheck_${LANG}.html
