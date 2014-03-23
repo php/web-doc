e0f010beb1a549789b47c3172a09f48ead6927ea
Sobak <msobaczewski@gmail.com>
2014-03-23 07:03:01 +0100

    Configure stuff and unused config vars removed

---
+++ build-ops-sample.php
@@ -0,0 +1,6 @@
+<?php
+// Please read REDAME.md in the same directory to get more info about filling in this file
+
+define('SVN_DIR',     '@SVNDIR@');
+define('DOC_DIR',     '@DOCDIR@'); // ?
+define('SQLITE_DIR',  '@SQLITEDIR@');
