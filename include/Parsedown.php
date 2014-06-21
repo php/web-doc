34539e84d6ff254063be10b6f65d881df17c33f2
Sobak <msobaczewski@gmail.com>
2014-06-21 10:00:10 +0200

    Upload license for parsedown

--- include/Parsedown.php
+++ include/Parsedown.php
@@ -8,7 +8,7 @@
 # (c) Emanuil Rusev
 # http://erusev.com
 #
-# For the full license information, view the LICENSE file that was distributed
+# For the full license information, view the PARSEDOWN_LICENSE file that was distributed
 # with this source code.
 #
 #
