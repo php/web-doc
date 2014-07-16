57234bcc359d7f3ad78b277e14e7ea43165237dd
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2014-07-16 00:35:06 -0300

    Fix typo REDAME -> README
    Signed-off-by: Rogerio Prado de Jesus <rogeriopradoj@gmail.com>

--- build-ops-sample.php
+++ build-ops-sample.php
@@ -1,5 +1,5 @@
 <?php
-// Please read REDAME.md in the same directory to get more info about filling in this file
+// Please read README.md in the same directory to get more info about filling in this file
 
 define('SVN_DIR',     '@SVNDIR@');
 define('SQLITE_DIR',  '@SQLITEDIR@');
